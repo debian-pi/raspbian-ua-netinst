dc94d5a559ce039255207b7fd4c8b75701997fec
Felix Rabe <felix@rabe.io>
2014-09-22 12:41:46 +0200

    buildroot.sh: Simplify

--- buildroot.sh
+++ buildroot.sh
@@ -20,25 +20,22 @@ b
 w
 EOF
 
+losetup_lt_2_22=false
+
 if ! losetup --version &> /dev/null ; then
   losetup_lt_2_22=true
 elif [ $(echo $(losetup --version | rev|cut -f1 -d' '|rev|cut -d'.' -f-2)'<'2.22 | bc -l) -ne 0 ]; then
   losetup_lt_2_22=true
-else
-  losetup_lt_2_22=false
 fi
 
-if [ "$losetup_lt_2_22" = "true" ] ; then
-
+if $losetup_lt_2_22 ; then
   kpartx -as $IMG
   mkfs.vfat /dev/mapper/loop0p1
   mount /dev/mapper/loop0p1 /mnt
   cp bootfs/* /mnt/
   umount /mnt
   kpartx -d $IMG || true
-
 else
-
   losetup -D
 
   losetup -P /dev/loop0 $IMG
@@ -47,7 +44,6 @@ else
   cp bootfs/* /mnt/
   umount /mnt
   losetup -D || true
-
 fi
 
 xz -9 $IMG
