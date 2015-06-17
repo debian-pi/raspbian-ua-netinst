14d3ec000053a5f0496c615d3c4731671f06e173
Diederik de Haas <github@cknow.org>
2015-06-17 14:01:10 +0200

    Don't be so quiet about setting up gpg in gnupg dir.

--- update.sh
+++ update.sh
@@ -86,7 +86,7 @@ setup_archive_keys() {
 
     mkdir -m 0700 -p gnupg
     # Let gpg set itself up already in the 'gnupg' dir before we actually use it
-    gpg -q --homedir gnupg --list-secret-keys &>/dev/null
+    gpg --homedir gnupg --list-secret-keys
 
     echo "Downloading ${RASPBIAN_ARCHIVE_KEY_FILE_NAME}."
     curl -# -O ${RASPBIAN_ARCHIVE_KEY_URL}
